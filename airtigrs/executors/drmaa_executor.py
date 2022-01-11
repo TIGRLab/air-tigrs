b3eb6c0bd48b834e37d1836cb103b440310a3ea1
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2022-01-11 15:12:38 -0500

    add job tracking using Airflow variable

--- airtigrs/executors/drmaa_executor.py
+++ airtigrs/executors/drmaa_executor.py
@@ -1,7 +1,7 @@
 import drmaa
 
 from typing import TYPE_CHECKING
-from typing import Optional, Tuple, Dict
+from typing import Optional, Tuple, Dict, List
 from multiprocessing import Queue, Empty
 
 from airflow.executors.base_executor import BaseExecutor, NOT_STARTED_MESSAGE
@@ -9,11 +9,14 @@ from airflow.exceptions import AirflowException
 
 import airflow.executors.config_adapters as adapters
 from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.utils.session import provide_session
+from airflow.models import Variable
 
 if TYPE_CHECKING:
     from airflow.models.taskinstance import (TaskInstanceKey,
                                              TaskInstanceStateType)
     from airflow.executors.base_executor import CommandType
+    from sqlalchemy.orm import Session
 
 # (key, command, job spec)
 DRMAAWorkType = Tuple[TaskInstanceKey, CommandType, Dict]
@@ -33,13 +36,54 @@ class DRMAAExecutor(BaseExecutor, LoggingMixin):
         self.task_queue: Optional[Queue[DRMAAWorkType]] = None
         self.session: Optional[drmaa.Session] = None
 
+    # TODO: Make `scheduler_job_ids` configurable under [executor]
+    @provide_session
+    def _get_or_create_job_var(self,
+                               session: Optional[Session] = None) -> List[int]:
+        current_jobs = Variable.get("scheduler_job_ids",
+                                    default_var=None,
+                                    deserialize_json=True)
+        if not current_jobs:
+            self.log.info("Setting up job tracking Airflow variable...")
+            Variable.set("scheduler_job_ids", {"jobs": []},
+                         description="Scheduler Job ID tracking",
+                         serialize_json=True,
+                         session=session)
+            self.log.info("Created `scheduler_job_ids` variable")
+
+        return current_jobs["jobs"]
+
+    @provide_session
+    def _push_job_id(self,
+                     job_id: int,
+                     session: Optional[Session] = None) -> None:
+
+        current_jobs = self._get_or_create_job_var()
+        current_jobs.append(job_id)
+        write_json = {"jobs": current_jobs}
+        Variable.update("scheduler_job_ids",
+                        write_json,
+                        serialize_json=True,
+                        session=session)
+
+    def push_to_tracking(self, job_id: int) -> None:
+        self.log.info(
+            "Adding Job {job_id} to tracking variable `scheduler_job_ids`")
+        self._push_job_id(job_id)
+        self.log.info("Successfully added {job_id} to `scheduler_job_ids`")
+
     def start(self) -> None:
         self.log.info("Initializing DRMAA session")
         self.session = drmaa.Session()
         self.session.initialize()
 
-        # Get cluster information?
-        # Use config?
+        self.log.info(
+            "Getting job tracking Airflow Variable: `scheduler_job_ids`")
+        current_jobs = self._get_or_create_job_var()
+
+        if current_jobs:
+            print_jobs = "\n".join([j_id for j_id in current_jobs["jobs"]])
+            self.log.info(f"Jobs from previous session:\n{print_jobs}")
 
     def end(self) -> None:
         self.log.info("Cleaning up remaining job statuses")
@@ -100,12 +144,10 @@ class DRMAAExecutor(BaseExecutor, LoggingMixin):
 
         # TODO: Use jobID to track information about running job
         # returned by following command
-        self.session.runJob(jt)
+        job_id = self.session.runJob(jt)
+        self.log.info(f"Submitted Job {job_id}")
+        self.push_to_tracking(job_id)
 
         # Prevent memory leaks on C back-end, running jobs unaffected
         # https://drmaa-python.readthedocs.io/en/latest/drmaa.html
         self.session.deleteJobTemplate(jt)
-
-        # Need to keep a record of the job for us to use later on
-        # needs to be persistent in case the scheduler/executor instance
-        # crashes
