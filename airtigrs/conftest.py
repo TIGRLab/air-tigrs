96259219e74f8d58470b5c10f544ca365d85fbb6
Michael Joseph <josephmje.22@gmail.com>
2022-02-17 14:01:49 -0500

    add dag tests

--- airtigrs/conftest.py
+++ airtigrs/conftest.py
@@ -1,7 +1,13 @@
 """py.test configuration"""
 
+from datetime import timedelta
 from pathlib import Path
 import pytest
+
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+
+pytest_plugins = ["helpers_namespace"]
 
 
 data_dir = Path(__file__).parent / "data" / "tests"
@@ -12,7 +18,24 @@ _config_data = {
 }
 
 
-@pytest.fixture()
+@pytest.fixture
 def dm_config_data(scope="session"):
     """Create a shared config dictionary across tests to pull data in."""
     return _config_data
+
+
+@pytest.fixture
+def test_dag():
+    """Airflow DAG for testing."""
+    return DAG(
+        "test_dag",
+        start_date=days_ago(1),
+        schedule_interval=timedelta(days=1),
+    )
+
+
+@pytest.helpers.register
+def run_task(task, dag):
+    """Run an Airflow task."""
+    dag.clear()
+    task.run(start_date=dag.start_date, end_date=dag.start_date)
---
+++ airtigrs/dags/tests/test_dag_integrity.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import pytest
+from airflow import models as airflow_models
+
+DAG_PATHS = Path(__file__).parents[1].glob("*.py")
+print(DAG_PATHS)
+
+
+@pytest.mark.parametrize("dag_path", DAG_PATHS)
+def test_dag_integrity(dag_path):
+    """Import DAG files and check for a valid DAG instance."""
+    dag_name = dag_path.name
+    module = _import_file(dag_name, dag_path)
+
+    # Validate if there is at least 1 DAG object in the file
+    dag_objects = [
+        var for var in vars(module).values() if isinstance(var, airflow_models.DAG)
+    ]
+    assert dag_objects
+
+    # For every DAG object, test for cycles
+    for dag in dag_objects:
+        dag.test_cycle()
+
+
+def _import_file(module_name, module_path):
+    import importlib.util
+
+    spec = importlib.util.spec_from_file_location(module_name, str(module_path))
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
