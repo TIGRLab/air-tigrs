9e40bc93eed73f7297adb26f8a8397afc52adad8
Michael Joseph <josephmje.22@gmail.com>
2022-03-17 12:07:53 -0400

    fix tests

--- airtigrs/dags/tests/test_dag_integrity.py
+++ airtigrs/dags/tests/test_dag_integrity.py
@@ -1,17 +1,20 @@
 """Test validity of all DAGs."""
-
+import os
 from pathlib import Path
 
 import pytest
 from airflow import models as airflow_models
 from airflow.utils.dag_cycle_tester import test_cycle as _test_cycle
 
+
 DAG_PATHS = Path(__file__).parents[1].glob("*.py")
 
-
 @pytest.mark.parametrize("dag_path", DAG_PATHS)
-def test_dag_integrity(dag_path):
+def test_dag_integrity(dag_path, dm_config_data):
     """Import DAG files and check for a valid DAG instance."""
+    os.environ["DM_CONFIG"] = str(dm_config_data["main_config"])
+    os.environ["DM_SYSTEM"] = "test"
+
     dag_name = dag_path.name
     module = _import_file(dag_name, dag_path)
 
